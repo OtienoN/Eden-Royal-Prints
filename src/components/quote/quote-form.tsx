6a86fc402f3432ce6390bac8a19fb1b181abdbe8
Mr. Cyber_Ke <mrcyberke@gmail.com>
2025-09-13 13:58:24 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/quote/quote-form.tsx
+++ src/components/quote/quote-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { useEffect } from "react";
 import { getQuote, type QuoteState } from "@/app/quote/actions";
 import { Button } from "@/components/ui/button";
--- src/components/store/order-form.tsx
+++ src/components/store/order-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { useEffect, useRef, useState } from "react";
 import { submitOrder, type OrderState } from "@/app/store/actions";
 import { useToast } from "@/hooks/use-toast";
